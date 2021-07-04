f166353d10076f4e54366a6b4808f000f9815c25
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-05 00:41:45 +0200

    [TEST CASES] TDD - TestCase Run PASSED - to read Person data from Json source File

--- src/main/java/com/safetynet/alerts/util/DataExchangerJsonToArrayList.java
+++ src/main/java/com/safetynet/alerts/util/DataExchangerJsonToArrayList.java
@@ -1,5 +1,53 @@
 package com.safetynet.alerts.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.alerts.model.Person;
+
+@Component
 public class DataExchangerJsonToArrayList {
 
+    private final List<Person> personList = new ArrayList<>();
+
+    @Value("${dataSourceJsonFilePath}")
+    private String dataFilePath;
+
+    @PostConstruct
+    public void readJsonDataToPOJO() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try (InputStream fileInputStream = new FileInputStream(dataFilePath)) {
+            JsonNode readJsonData = mapper.readTree(fileInputStream);
+
+            // Transfers the all person details in the corresponding array list
+            JsonNode persons = readJsonData.at("/persons");
+            for (JsonNode jsonNode : persons) {
+                Person person = new Person(jsonNode.get("firstName").asText(),
+                        jsonNode.get("lastName").asText(),
+                        jsonNode.get("address").asText(),
+                        jsonNode.get("city").asText(),
+                        jsonNode.get("zip").asInt(),
+                        jsonNode.get("phone").asText(),
+                        jsonNode.get("email").asText());
+
+                personList.add(person);
+            }
+         }
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
 }
--- src/test/java/com/safetynet/alerts/IT/util/DataExchangerJsonToArrayListIT.java
+++ src/test/java/com/safetynet/alerts/IT/util/DataExchangerJsonToArrayListIT.java
@@ -1,22 +1,21 @@
 package com.safetynet.alerts.IT.util;
 
-import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.safetynet.alerts.model.FireStation;
-import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.util.DataExchangerJsonToArrayList;
 
+@DisplayName("IT - Reading JsonFile To ArrayList")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class DataExchangerJsonToArrayListIT {
@@ -24,6 +23,10 @@ public class DataExchangerJsonToArrayListIT {
     @Autowired
     private DataExchangerJsonToArrayList dataExchangerJsonToArrayList;
 
+    @DisplayName("ReadingJsonInputFile for PERSON"
+    		+ " - Given input Json File to read"
+    		+ "When get Person List"
+    		+ "then return expected number of entries in the list")
     @Test
     public void whenGetPersonList_thenReturnExpectedPersonList() {
         List<Person> listOfPersons = dataExchangerJsonToArrayList.getPersonList();
