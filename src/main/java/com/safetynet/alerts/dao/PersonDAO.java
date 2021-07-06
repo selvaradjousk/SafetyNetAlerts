b63db66d9baab9b0273881270b2ee36018eee7cc
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-06 11:52:08 +0200

    [FEAT] Person Service - to Get Person List- introduced - Test Run Status: PASSED

--- src/main/java/com/safetynet/alerts/dao/PersonDAO.java
+++ src/main/java/com/safetynet/alerts/dao/PersonDAO.java
@@ -1,8 +1,17 @@
 package com.safetynet.alerts.dao;
 
+import java.util.List;
+
+import com.safetynet.alerts.model.Person;
+
 public class PersonDAO {
 
-	public Object getPersonList() {
+	public List<Person> getPersonList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Person getPersonByName(String firstName, String lastName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/main/java/com/safetynet/alerts/service/PersonEndpointService.java
+++ src/main/java/com/safetynet/alerts/service/PersonEndpointService.java
@@ -2,23 +2,38 @@ package com.safetynet.alerts.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.safetynet.alerts.dao.PersonDAO;
 import com.safetynet.alerts.dto.PersonEndpointDTO;
 import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.util.DataExchangerModelToDTO;
 
 @Service
 public class PersonEndpointService {
 
+	private PersonDAO personDAO;
 
-    public PersonEndpointDTO getPersonById(String firstName, String lastName) {
-        return null;
+	private DataExchangerModelToDTO dataExchangerModelToDTO;
+	
+    @Autowired
+    public PersonEndpointService(
+    		final PersonDAO personDAO,
+    		final DataExchangerModelToDTO dataExchangerModelToDTO) {
+        this.personDAO = personDAO;
+        this.dataExchangerModelToDTO = dataExchangerModelToDTO;
     }
+	
+    public PersonEndpointDTO getPersonById(String firstName, String lastName) {
 
-	public List<Person> getAllPersonList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        Person person = personDAO.getPersonByName(firstName, lastName);
+        return dataExchangerModelToDTO.transferToPersonEndpointDTO(person);
+    }
     
+    public List<Person> getAllPersonList() {
+        List<Person> listOfPerson = personDAO.getPersonList();
+       return listOfPerson;
+   }
 
 }
