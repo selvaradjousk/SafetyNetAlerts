e91c5395da49fec2c49875d2f0d5d2c0d3e12aac
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-11 00:09:20 +0200

    [TEST + FEAT] update PersonService Add Person with Exception Unit Test Case - Person Already Exists Check + screen capture for work progress tracing

---
+++ src/main/java/com/safetynet/alerts/exception/DataAlreadyRegisteredException.java
@@ -0,0 +1,10 @@
+package com.safetynet.alerts.exception;
+
+
+public class DataAlreadyRegisteredException extends RuntimeException {
+
+    public DataAlreadyRegisteredException(final String message) {
+        super(message);
+    }
+}
+
