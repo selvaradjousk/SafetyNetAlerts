fd1ff20aacfaf6f9960eae48efdd889805cf266a
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-10 22:43:44 +0200

    [FEAT] Person Controller ADD NEW PERSON introducted - Run Status: PASSED

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,13 +27,32 @@ public class PersonController {
 	public ResponseEntity<PersonDTO> getPerson(@RequestParam("firstName") final String firstName,
 			@RequestParam("lastName") final String lastName) {
 
-        if (firstName == null || firstName.trim().length() == 0 || lastName == null
+        if (firstName == null
+        		|| firstName.trim().length() == 0
+        		|| lastName == null
                 || lastName.trim().length() == 0) {
             throw new BadRequestException("Response Status: 400 Bad Request"
             		+ " -> The parameter entered has missing values or invalid");
         }
-		PersonDTO personDTO = personEndpointService.getPersonById(firstName, lastName);
+		PersonDTO personDTO = personEndpointService
+				.getPersonById(firstName, lastName);
 
 		return new ResponseEntity<>(personDTO, HttpStatus.OK);
 	}
+	
+    @PostMapping("/person")
+    public ResponseEntity<PersonDTO> addNewPerson(@RequestBody final PersonDTO person) {
+
+        if (person.getFirstName() == null
+        		|| person.getFirstName().isEmpty()
+        		|| person.getLastName() == null
+                || person.getLastName().isEmpty()) {
+            throw new BadRequestException("Response Status: 401 Bad request "
+            		+ "The request body is incomplete or missing required entries");
+        }
+        PersonDTO personAdded = personEndpointService
+        		.addNewPerson(person);
+
+        return new ResponseEntity<>(personAdded, HttpStatus.CREATED);
+    }
 }
