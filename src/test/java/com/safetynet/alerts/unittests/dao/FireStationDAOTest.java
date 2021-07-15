c14e7b2676136bd428f912e194da0d4a20750b8d
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-16 01:14:58 +0200

    [Info Report] FireStation SERVICE - Test Runs Completed Junit Report

---
+++ src/test/java/com/safetynet/alerts/unittests/dao/FireStationDAOTest.java
@@ -0,0 +1,14 @@
+package com.safetynet.alerts.unittests.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FireStationDAOTest {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
