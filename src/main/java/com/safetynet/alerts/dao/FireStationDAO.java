7f494cddea385c5110c70c7611d8fdfe209dce6a
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-16 21:07:43 +0200

    [TEST CASE] FireStation DAO - GET BY STATION ID - Test Run Status: FAILED

--- src/main/java/com/safetynet/alerts/dao/FireStationDAO.java
+++ src/main/java/com/safetynet/alerts/dao/FireStationDAO.java
@@ -1,14 +1,28 @@
 package com.safetynet.alerts.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.safetynet.alerts.model.FireStation;
+import com.safetynet.alerts.util.DataFileReader;
 
 @Repository
 public class FireStationDAO implements IFireStationDAO {
 
+	private final Map<String, FireStation> fireStationMap = new HashMap<>();
+	
+	
+	   @Autowired
+	    public FireStationDAO(final DataFileReader jsonDataArrayList) {
+	        jsonDataArrayList.getFireStationList()
+	        .forEach(fireStation -> fireStationMap
+	        		.put(fireStation.getStationId()
+	        		 + fireStation.getAddress(), fireStation));
+	    }
 
 	public List<FireStation> getStationsByStationIds(int stationId) {
 		// TODO Auto-generated method stub
--- src/test/java/com/safetynet/alerts/unittests/dao/FireStationDAOTest.java
+++ src/test/java/com/safetynet/alerts/unittests/dao/FireStationDAOTest.java
@@ -1,14 +1,71 @@
 package com.safetynet.alerts.unittests.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-class FireStationDAOTest {
+import com.safetynet.alerts.dao.FireStationDAO;
+import com.safetynet.alerts.dao.IFireStationDAO;
+import com.safetynet.alerts.model.FireStation;
+import com.safetynet.alerts.util.DataFileReader;
 
-	@Test
-	final void test() {
-		fail("Not yet implemented"); // TODO
-	}
+@DisplayName("FireStation DAO - Unit Tests")
+@ExtendWith(MockitoExtension.class)
+public class FireStationDAOTest {
 
+    private IFireStationDAO iFireStationDAO;
+
+    private static FireStation firestation1;
+    private static FireStation firestation2;
+
+    @Mock
+    private DataFileReader dataFileReader;
+
+    @BeforeEach
+    public void setUp() {
+        firestation1 = new FireStation(2, "Test1 StreetName");
+        firestation2 = new FireStation(1, "Test2 StreetName");
+
+        when(dataFileReader
+        		.getFireStationList())
+        .thenReturn(Arrays.asList(firestation1, firestation2));
+
+        iFireStationDAO = new FireStationDAO(dataFileReader);
+    }
+
+    
+    // ***********************************************************************************
+
+    @DisplayName("Test GET BY STATION ID")
+    @Nested
+    class TestGetFireStationById {  
+        @BeforeEach
+        public void init() {
+        }
+    
+    @Test
+    @DisplayName("Check (valid Station for Expected Value Match)"
+    		+ " - Given an expected FireStation,"
+    		+ " when get firestation,"
+    		+ " then fireStation found should match expected FireStation")
+    public void testGetFireStationById() {
+        FireStation fireStationFound = iFireStationDAO
+        		.getStationById(firestation2.getStationId(), firestation2.getAddress());
+
+        assertEquals(firestation2, fireStationFound);
+    }
+    
+    }
+    
 }
