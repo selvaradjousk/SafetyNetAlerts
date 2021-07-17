f68b3a023b03fba3690465ce4cac8fdcd010f032
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-18 00:02:40 +0200

    [FEAT] Controller - GET MEDICAL RECORD - Check on valid input - TestRun: PASSED

--- src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
+++ src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
@@ -1,8 +1,14 @@
 package com.safetynet.alerts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.safetynet.alerts.dto.MedicalRecordDTO;
+import com.safetynet.alerts.exception.BadRequestException;
 import com.safetynet.alerts.service.IMedicalRecordService;
 
 @RestController
@@ -16,4 +22,18 @@ public class MedicalRecordController {
        this.medicalRecordService = medicalRecordService;
    }
 
+   @GetMapping("/medicalRecord")
+   public ResponseEntity<MedicalRecordDTO> getMedicalRecord(@RequestParam("firstName") final String firstName,
+                                                            @RequestParam("lastName") final String lastName) {
+
+       if (firstName == null || firstName.trim().length() == 0 || lastName == null
+               || lastName.trim().length() == 0) {
+           throw new BadRequestException("Bad request : missing or incomplete parameter");
+       }
+       MedicalRecordDTO medDTO = medicalRecordService.getMedicalRecordById(firstName, lastName);
+
+       return new ResponseEntity<>(medDTO, HttpStatus.OK);
+   }
+   
+   
 }
