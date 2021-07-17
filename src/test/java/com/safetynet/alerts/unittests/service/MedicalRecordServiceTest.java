83d3a084b18f0d200f657af80d0b87f3073b90ff
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-18 01:21:00 +0200

    [TEST CASE] MedicalRecord Service - Get MedicalRecord - TestRun: FAILED

--- src/test/java/com/safetynet/alerts/unittests/service/MedicalRecordServiceTest.java
+++ src/test/java/com/safetynet/alerts/unittests/service/MedicalRecordServiceTest.java
@@ -1,14 +1,113 @@
 package com.safetynet.alerts.unittests.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-class MedicalRecordServiceTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.alerts.dao.MedicalRecordDAO;
+import com.safetynet.alerts.dto.MedicalRecordDTO;
+import com.safetynet.alerts.model.MedicalRecord;
+import com.safetynet.alerts.service.MedicalRecordService;
+import com.safetynet.alerts.util.MedicalRecordMapper;
 
-	@Test
-	final void test() {
-//		fail("Not yet implemented"); // TODO
-	}
+
+@DisplayName("Medical Record Service - Unit Tests")
+@ExtendWith(MockitoExtension.class)
+public class MedicalRecordServiceTest {
+
+    @Mock
+    private MedicalRecordDAO medicalRecordDAOMock;
+
+    @Mock
+    private MedicalRecordMapper medicalRecordMapper;
+
+    @InjectMocks
+    private MedicalRecordService medicalRecordService;
+
+    @Mock
+    private static MedicalRecord medicalRecord;
+
+    @Mock
+    private static MedicalRecordDTO medicalRecordDTO;
+
+	private ObjectMapper objectMapper;
+	
+	MedicalRecordDTO medicalRecordAdded, medicalRecordUpdated, medicalRecordByIdFound;
+    
+    @BeforeEach
+    public void setUp() {
+        objectMapper = new ObjectMapper();
+        medicalRecordDTO = MedicalRecordDTO.builder()
+        		.firstName("Test FirstName")
+        		.lastName("Test Last Name")
+        		.birthDate("01/01/1960")
+        		.medications(Arrays.asList(
+        				"some medication1",
+        				"some medicati2n1",
+        				"some medicati3n1"))
+                .allergies(Arrays.asList(
+                		"some allergies1",
+                		"some allergies2",
+                		"some allergies3"))
+                .build();
+        
+        medicalRecord = MedicalRecord.builder()
+        		.firstName("Test FirstName")
+        		.lastName("Test Last Name")
+        		.birthDate("01/01/1960")
+        		.medications(Arrays.asList(
+        				"some medication1",
+        				"some medicati2n1",
+        				"some medicati3n1"))
+                .allergies(Arrays.asList(
+                		"some allergies1",
+                		"some allergies2",
+                		"some allergies3"))
+                .build();
+    }
+    
+
+    // ***********************************************************************************
+    @DisplayName("Test GET MEDICAL RECORD BY ID")
+    @Nested
+    class TestDeleteMedicalRecord {
+    	
+        @BeforeEach
+        public void init() {
+            when(medicalRecordDAOMock
+            		.getMedicalRecordByPersonId(anyString(), anyString()))
+            .thenReturn(medicalRecord);
+            when(medicalRecordMapper
+            		.toMedicalRecordDTO(any(MedicalRecord.class)))
+            .thenReturn(medicalRecordDTO);
+        }	
+	        @Test
+        @DisplayName("Check <Not Null> on Get Record"
+        		+ " - Given a medicalRecord WITH PERSON ID,"
+        		+ " when request get MedicalRecord,"
+        		+ " then returned medical record not null")
+        public void testGetMedicalRecordByIdNotNullCheck() {
+
+            medicalRecordByIdFound = medicalRecordService
+            		.getMedicalRecordById(medicalRecordDTO.getFirstName(),
+                    medicalRecordDTO.getLastName());
+
+            assertNotNull(medicalRecordByIdFound);
+        }  
+    
+    }
 
 }
