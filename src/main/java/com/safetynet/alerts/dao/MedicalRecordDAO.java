fb1b6ab324396a087271bca80af723d274cd395d
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-07-18 11:07:37 +0200

    [TEST CASES] MedicalRecord DAO - GET MedicalRecord - valid input - TestRun: FAILED

--- src/main/java/com/safetynet/alerts/dao/MedicalRecordDAO.java
+++ src/main/java/com/safetynet/alerts/dao/MedicalRecordDAO.java
@@ -1,12 +1,25 @@
 package com.safetynet.alerts.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.safetynet.alerts.model.MedicalRecord;
+import com.safetynet.alerts.util.DataFileReader;
 
 @Repository
 public class MedicalRecordDAO implements IMedicalRecordDAO {
 
+	
+	private final Map<String, MedicalRecord> medicalRecordsMap = new HashMap<>();
+	
+    @Autowired
+    public MedicalRecordDAO(final DataFileReader jsonDataArrayList) {
+                jsonDataArrayList.getMedicalRecordList().forEach(medicalRecord -> medicalRecordsMap.put(medicalRecord.getFirstName()
+                + medicalRecord.getLastName(), medicalRecord));
+    }
 	public MedicalRecord updateMedicalRecord(MedicalRecord medicalRecord) {
 		// TODO Auto-generated method stub
 		return null;
--- src/test/java/com/safetynet/alerts/unittests/dao/MedicalRecordDAOTest.java
+++ src/test/java/com/safetynet/alerts/unittests/dao/MedicalRecordDAOTest.java
@@ -1,14 +1,74 @@
 package com.safetynet.alerts.unittests.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-class MedicalRecordDAOTest {
+import com.safetynet.alerts.dao.IMedicalRecordDAO;
+import com.safetynet.alerts.dao.MedicalRecordDAO;
+import com.safetynet.alerts.model.MedicalRecord;
+import com.safetynet.alerts.util.DataFileReader;
 
-	@Test
-	final void test() {
-//		fail("Not yet implemented"); // TODO
-	}
+@DisplayName("Medical Record DAO - Unit Tests")
+@ExtendWith(MockitoExtension.class)
+public class MedicalRecordDAOTest {
 
+    private IMedicalRecordDAO iMedicalRecordDAO;
+
+    @Mock
+    private DataFileReader dataFileReader;
+
+    private static MedicalRecord medicalRecord1, medicalRecord2,
+    medicalRecord3, medicalRecordFound, medicalRecordSaved;
+    
+    @BeforeEach
+    public void setUp() {
+    	
+    	medicalRecord1 = new MedicalRecord(
+    			"Test1 FirstName",
+    			"Test1 LastName",
+    			"01/01/1976",
+                Arrays.asList("medications 1"),
+                Arrays.asList("alergies 1"));
+    	
+        medicalRecord2 = new MedicalRecord(
+        		"Test2 FirstName",
+        		"Test2 LastName",
+        		"01/01/2005",
+        		Arrays.asList("medication2-1"),
+        		Arrays.asList("allergies2-1"));
+
+        when(dataFileReader
+        		.getMedicalRecordList())
+        .thenReturn(Arrays.asList(medicalRecord1, medicalRecord2));
+
+        iMedicalRecordDAO = new MedicalRecordDAO(dataFileReader);
+    }
+    
+    
+    @Test
+    @DisplayName("Check (valid id )"
+    		+ "Given a person Id,"
+    		+ " when getMedicalRecordByPersonId,"
+    		+ " then medicalRecord returned")
+    public void testGetMedicalRecordByPersonIdForValidInput() {
+        medicalRecordFound = iMedicalRecordDAO
+        		.getMedicalRecordByPersonId(
+        				"Test1 FirstName",
+        				"Test1 LastName");
+
+        assertEquals(medicalRecord1, medicalRecordFound);
+    }
+ 
+    
+    
 }
